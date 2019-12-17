f6564f11df5fad01152f6aacd60a5f86324f014a
Verkeys Francis <36914841+verfran@users.noreply.github.com>
2019-12-17 22:57:25 +0530

    Update addMembers.py
    addMembers to read dobaptism, dobirth, matatus, contact

--- FRNChurch/FRNChurchStructure/management/commands/addMembers.py
+++ FRNChurch/FRNChurchStructure/management/commands/addMembers.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand, CommandError
 from openpyxl import load_workbook
-from FRNChurchStructure.models import Group, Christian
+from FRNChurchStructure.models import Group, Christian, Contact
 
 class Command(BaseCommand):
     help = 'addMembers: This command will add members to the Church groups.\
@@ -17,7 +17,7 @@ class Command(BaseCommand):
         activeSheet = wb.active
 
         if not self.isValid(activeSheet[1]):
-            return;
+            return
 
         try:
             rgn = Group.objects.get(name=self.regionName)
@@ -25,7 +25,6 @@ class Command(BaseCommand):
             print("Error: Region not found ", self.regionName)
             print("Error: Create region without type manually and then try addMembers")
             return
-
 
         rowCount = activeSheet.max_row
         for rowindex in range(2, rowCount+1):
@@ -38,9 +37,23 @@ class Command(BaseCommand):
         zone = self.getGroupUnder(row[3].value, 'Zone', sector)
         fg = self.getGroupUnder(row[4].value, 'Family Group', zone)
 
-        self.createChristian(fg, row[5].value, row[6].value)
+        newCtn = self.createChristian(fg, row[5].value, row[6].value, row[7].value,
+            row[8].value, row[9].value)
+        self.updateContact(newCtn, row[10].value, row[11].value)
 
-    def createChristian(self, fg, name, gender):
+    def updateContact(self, ctn, phone, email):
+        if phone == None and email == None:
+            return
+        contact = Contact()
+        if phone != None:
+            contact.phoneNumber = phone
+        if email != None:
+            contact.email = email
+        contact.save()
+        ctn.contact = contact
+        ctn.save()
+
+    def createChristian(self, fg, name, gender, dobirth, dobaptism, mstatus):
         chrtn = Christian()
         chrtn.familyGroup = fg
         names = name.split(" ", 1)
@@ -48,6 +61,14 @@ class Command(BaseCommand):
         if len(names) == 2:
             chrtn.secondName = names[1]
         chrtn.gender = gender
+        chrtn.dateOfBirth = dobirth
+        chrtn.dateOfBaptism = dobaptism
+        if "Married" in mstatus or "Parent" in mstatus:
+            chrtn.maritalStatus = 'M'
+        if "Widow" in mstatus:
+            chrtn.maritalStatus = 'W'
+        if "Divor" in mstatus:
+            chrtn.maritalStatus = 'D'
         chrtn.save()
         print('New Christian %s \'%s\' created and added under %s %s'
             %(chrtn.firstName, chrtn.gender, fg.type, fg.name))
@@ -97,6 +118,21 @@ class Command(BaseCommand):
             return False
         if "Gender" != title[6].value:
             print("Error: Invalid xls file: Seventh colum should have Gender")
+            return False
+        if "DOBirth" != title[7].value:
+            print("Error: Invalid xls file: Seventh colum should have DOBirth")
+            return False
+        if "DOBaptism" != title[8].value:
+            print("Error: Invalid xls file: Seventh colum should have DOBaptism")
+            return False
+        if "Marital Status" != title[9].value:
+            print("Error: Invalid xls file: Seventh colum should have Marital Status")
+            return False
+        if "Phone Number" != title[10].value:
+            print("Error: Invalid xls file: Seventh colum should have Phone Number")
+            return False
+        if "Email" != title[11].value:
+            print("Error: Invalid xls file: Seventh colum should have Email")
             return False
 
         return True
